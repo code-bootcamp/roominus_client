09d002b06e7135fb6e29b51c2fb532c83e4f85a2
eunchae <104378330+dev-eun-chae@users.noreply.github.com>
2022-07-05 19:33:39 +0900

    [추가] feature#37 예약페이지 추가

---
+++ src/components/units/reservation/Reservation.styles.ts
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import { breakPoints } from "../../../commons/styles/media";
+
+export const Wrapper = styled.section`
+  margin-top: 4em;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media ${breakPoints.mobile} {
+    width: 100%;
+    height: 100%;
+    font-size: 0.5em;
+  }
+`;
+
+export const Form = styled.form`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2em;
+`;
+
+export const SelectBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputBox = styled.div``;
+export const InputLabel = styled.label``;
+export const Input = styled.input`
+  width: 100%;
+  height: 2em;
+  border: none;
+  border-bottom: 1px solid lightgray;
+  outline: none;
+  font-size: 1.3em;
+`;
+
+export const Error = styled.div`
+  font-size: 1.2em;
+  color: red;
+  margin-bottom: 2em;
+`;
+export const PasswordBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+`;
+
+export const CancelBtn = styled.button`
+  width: 100%;
+  height: 4em;
+  border-radius: 10px;
+  font-size: 1em;
+  border: none;
+  color: white;
+  background-color: #b2b2b2;
+  cursor: pointer;
+`;
+export const SubmitBtn = styled.button`
+  width: 100%;
+  height: 4em;
+  border-radius: 10px;
+  font-size: 1em;
+  border: none;
+  color: white;
+  background-color: #4a00e0;
+  cursor: pointer;
+`;
