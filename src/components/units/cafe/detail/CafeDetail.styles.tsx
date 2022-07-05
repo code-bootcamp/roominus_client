ad0886b1a824d8c54098ad3b1295be21d7437f07
hmds1606 <hmds1606@gmail.com>
2022-07-05 19:33:28 +0900

    [추가] feature#34 cafe 상세페이지

---
+++ src/components/units/cafe/detail/CafeDetail.styles.tsx
@@ -0,0 +1,73 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.main`
+  width: 100%;
+`;
+
+export const SearchWrapper = styled.section`
+  width: 100%;
+  /* background-color: #bdbdbd; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CafePicture = styled.div`
+  width: 38em;
+  height: 25em;
+  background-color: pink;
+`;
+
+export const InfoBox = styled.div`
+  width: 38em;
+  /* height: 44em; */
+  padding: 1em 3em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: #bdbdbd;
+`;
+
+export const NamePhoneBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CafeName = styled.h1``;
+
+export const CafePhone = styled.div``;
+
+export const MenuBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Button = styled.button`
+  width: 8em;
+  height: 2.5em;
+  border: none;
+  margin-right: 1em;
+`;
+
+export const InfoContentsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ContentsTitle = styled.h3``;
+
+export const Contents = styled.div``;
+
+export const Map = styled.div`
+  width: 33em;
+  height: 13.7em;
+  background-color: blue;
+`;
+
+export const LocationBox = styled.div``;
+
+export const Location = styled.div``;
+
+export const LocationDetail = styled.div``;
