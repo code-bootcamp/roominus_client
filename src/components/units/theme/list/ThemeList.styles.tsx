691318dd9b4a150a83876b48d87075f637b8f4cd
haneebunny <re____@naver.com>
2022-07-07 17:19:28 +0900

    [수정] 테마 리스트 수정 -  flip효과

--- src/components/units/theme/list/ThemeList.styles.tsx
+++ src/components/units/theme/list/ThemeList.styles.tsx
@@ -10,7 +10,6 @@ export const Container = styled.main`
   width: 100vw;
   height: 1000px;
   position: relative;
-
   left: calc(-50vw + 50%);
 
   display: flex;
@@ -25,12 +24,16 @@ export const Container = styled.main`
   }
 `;
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  width: 100%;
+  padding: 10%;
+`;
 
 export const GenreList = styled.div`
   @media ${breakPoints.mobile} {
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(110px, auto));
+    grid-gap: 0px 30px;
     align-items: center;
   }
 `;
@@ -47,34 +50,142 @@ export const Genre = styled.button`
   margin: 5px;
 `;
 
-export const ThemeList = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-columns: repeat(auto-fill, minmax(20%, auto));
-  @media ${breakPoints.mobile} {
-    display: flex;
-    flex-direction: column;
+// -------------------------------
+export const FlipDiv = styled.div`
+  width: 200px;
+  height: 250px;
+  position: relative;
+  perspective: 1100px;
+  margin: 2rem;
+  :hover {
+    transform: rotateY(180deg);
   }
 `;
 
-export const Theme = styled.li`
+export const CardDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: 0.4s;
+  transform-style: preserve-3d;
+  :hover {
+    transform: rotateY(180deg);
+  }
+`;
+
+export const FrontBackDiv = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+`;
+
+export const FrontDiv = styled(FrontBackDiv)`
+  background-color: red;
+`;
+
+export const BackDiv = styled(FrontBackDiv)`
+  background-color: blue;
+  transform: rotateY(180deg);
+`;
+// -------------------------------
+
+export const ThemeList = styled.div`
+  padding: 20px 0px;
+  display: grid;
+  /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+  grid-template-columns: repeat(auto-fill, minmax(290px, auto));
+  /* grid-template-rows: repeat(auto-fill, minmax(420px, auto)); */
+  /* grid-template-rows: repeat(3, minmax(400px, auto)); */
+  row-gap: 30px;
+  @media ${breakPoints.mobile} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const Flip = styled.div`
   width: 290px;
   height: 420px;
+  position: relative;
+  perspective: 1100px;
+
+  :hover {
+    /* transform: rotateY(180deg); */
+  }
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: 0.4s;
+  transform-style: preserve-3d;
+
+  :hover {
+    transform: rotateY(180deg);
+  }
+`;
+
+export const defaultDiv = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  display: flex;
+`;
+
+export const Theme = styled(defaultDiv)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /* width: 290px;
+  height: 420px; */
   padding: 10px 15px;
-  background: url(${(props: IGenreProps) => props.src});
   display: flex;
   flex-direction: column;
   justify-content: end;
+  background-blend-mode: multiply;
+  background: linear-gradient(
+      180deg,
+      rgba(38, 40, 44, 0) 0%,
+      rgba(38, 40, 44, 0.14) 56.77%,
+      #26282c 100%
+    ),
+    url(${(props: IGenreProps) => props.src});
 `;
 
-export const ThemeImg = styled.img`
-  width: 290px;
+export const ThemeImg = styled(defaultDiv)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /* width: 290px;
+  height: 420px; */
+
+  background: url(${(props: IGenreProps) => props.src});
+  transform: rotateY(180deg);
+`;
+
+export const ImgGradient = styled.div`
+  /* width: 290px;
   height: 420px;
+  background: linear-gradient(
+    180deg,
+    rgba(38, 40, 44, 0) 0%,
+    rgba(38, 40, 44, 0.14) 56.77%,
+    #26282c 100%
+  );
+  background-blend-mode: multiply; */
 `;
 
 export const GenreTag = styled.div`
-  width: 140px;
-  height: 45px;
+  width: 130px;
+  height: 40px;
   border-radius: 8px;
   background-color: #492396b0;
   font-size: 24px;
@@ -86,6 +197,7 @@ export const GenreTag = styled.div`
 `;
 
 export const Rank = styled.h4`
+  padding: 5px;
   color: white;
   display: flex;
   flex-direction: row;
