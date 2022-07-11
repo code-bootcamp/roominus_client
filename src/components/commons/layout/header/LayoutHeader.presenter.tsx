bec131797add35a53bae41c774c2a649a3de1e08
haneebunny <re____@naver.com>
2022-07-11 19:27:20 +0900

    [수정]feature#170 로고, 헤더 활성화 표시되게끔 수정

--- src/components/commons/layout/header/LayoutHeader.presenter.tsx
+++ src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -10,6 +10,55 @@ const NAVIGATION_MENUS = [
   { name: "마이페이지", page: "/mypage" },
 ];
 
+const styles = {
+  bmBurgerButton: {
+    position: "absolute",
+    width: "1.5em",
+    height: "1.5em",
+    right: "2.5em",
+    top: "2.3em",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    right: "0px",
+    top: "0px",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#6c5281",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+
+  bmItemList: {
+    color: "#60046f",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "block",
+    color: "#d1d1d1",
+  },
+  bmOverlay: {
+    background: "rgba(116, 87, 151, 0.3)",
+    position: "fixed", //SearchWrapper만 화면 끝까지 늘리기
+    top: "0",
+    left: "calc(-100vw + 100%)",
+  },
+};
+
 export default function LayoutHeaderUI(props: any) {
   return (
     <>
