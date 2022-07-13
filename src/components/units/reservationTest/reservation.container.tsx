599ac447c60324f5c9f389c2bf0d9b3b223bed01
eunchae <104378330+dev-eun-chae@users.noreply.github.com>
2022-07-13 11:45:28 +0900

    [수정] 아이콘 수정

--- src/components/units/reservationTest/reservation.container.tsx
+++ src/components/units/reservationTest/reservation.container.tsx
@@ -6,34 +6,56 @@ export default function ReservationTest() {
   const [theme, setTheme] = useState("");
   const [cafe, setCafe] = useState("");
   const [selectedDate, setSelectedDate] = useState();
+
   // const [inputValue, setInputValue] = useState(moment().format("YYYY-MM-DD"));
+
   const [time, setTime] = useState("");
   const [headCount, setHeadCount] = useState("");
-
-  const onChangeTheme = (event) => {
-    setTheme(event.target.value);
-  };
-
-  const onChangeCafe = (event: any) => {
-    setCafe(event.target.value);
-    console.log(cafe);
-  };
-
-  const onChangeDate = (date: String, value: String) => {
-    setSelectedDate(date);
-    // setInputValue(value);
-  };
+  const [price, setPrice] = useState(0);
+  const [point, setPoint] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const dateFormatter = (str: any) => {
     return str;
   };
 
+  const onChangeTheme = (event) => {
+    setTheme(event.target.value);
+    console.log("theme:", theme);
+  };
+
+  const onChangeCafe = (event: any) => {
+    setCafe(event.target.value);
+    console.log("cafe:", cafe);
+  };
+
+  const onChangeDate = (date: String, value: String) => {
+    setSelectedDate(date);
+    // setInputValue(value);
+    console.log("date:", date);
+
+    // String 포맷으로 저장할 때 dayjs 사용할 것
+  };
+
   const onChangeTime = (event: any) => {
     setTime(event.target.value);
+    console.log("time:", time);
   };
 
   const onChangeHeadCount = (event) => {
     setHeadCount(event.target.value);
+    setPrice(event.target.value * 12000);
+    setTotalPrice(price);
+
+    console.log("price:", price);
+  };
+
+  const onChangePoint = (event) => {
+    setPoint(event.target.value);
+    console.log("point:", point);
+
+    setTotalPrice(price - point);
+    console.log("total:", totalPrice);
   };
 
   return (
@@ -46,10 +68,13 @@ export default function ReservationTest() {
       selectedDate={selectedDate}
       // inputValue={inputValue}
       dateFormatter={dateFormatter}
-      onChangeTime={onChangeTime}
       time={time}
+      onChangeTime={onChangeTime}
       haedCount={headCount}
       onChangeHeadCount={onChangeHeadCount}
+      point={point}
+      onChangePoint={onChangePoint}
+      totalPrice={totalPrice}
     />
   );
 }
