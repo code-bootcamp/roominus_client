f1cf075e98774f25a4f5d4d9a3441256b8f26863
hmds1606 <hmds1606@gmail.com>
2022-07-13 22:15:29 +0900

    [추가] feature#213 community 등록페이지 기능 구현

---
+++ src/components/commons/tag/Tag.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect } from "react";
+import styled from "@emotion/styled";
+
+export default function Tag(props: any) {
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      console.log(props.tagList);
+      //처음에 입력한 값
+      if (props.tagList === undefined) {
+        props.setTagList([e.target.value]);
+        props.setTagItem("");
+      } else {
+        //추가로 입력한 값
+        props.setTagList((prev) => [...prev, e.target.value]);
+        // addList.filter((tagList: any) => tagList !== e.target.value);
+        props.setTagItem("");
+      }
+    }
+  };
+
+  const deleteTagItem = (e: any) => {
+    const deleteTagItem = e.target.parentElement.firstChild.innerText;
+    const filteredTagList = props.tagList.filter(
+      (tagItem: any) => tagItem !== deleteTagItem
+    );
+    props.setTagList(filteredTagList);
+  };
+
+  const onChangeInput = (e: any) => {
+    props.setTagItem(e.target.value);
+  };
+
+  //수정시 map대신 useEffect
+  useEffect(() => {
+    if (props.marketData?.fetchUseditem !== undefined)
+      props.setTagList([...props.marketData?.fetchUseditem.tags]);
+  }, [props.marketData]);
+
+  return (
+    <TagBox>
+      {/* 등록시 map */}
+      {props.tagList?.map((tagItem: any, index: number) => {
+        const hashedtag = tagItem.replace(tagItem, `#${tagItem}`);
+        return (
+          <TagItem key={index}>
+            {/* <span>#</span> */}
+            <Text>{hashedtag}</Text>
+            <Button type="button" onClick={deleteTagItem}>
+              X
+            </Button>
+          </TagItem>
+        );
+      })}
+      <TagInput
+        type="text"
+        placeholder="태그를 입력해주세요"
+        tabIndex={2}
+        onChange={onChangeInput}
+        value={props.tagItem}
+        onKeyPress={onKeyPress}
+      />
+    </TagBox>
+  );
+}
+
+const TagBox = styled.div`
+  width: 100%;
+  /* height: 100%; */
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  border-radius: 5px;
+  border: 1px solid #a3a8b7;
+  box-shadow: 0px 4px rgba(217, 214, 224, 0.25);
+  margin-bottom: 1em;
+
+  /* padding: 0 10px; */
+  /* background-color: pink; */
+
+  /* &:focus-within {
+    border-color: tomato;
+  } */
+`;
+
+const TagItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5px;
+  padding: 0.4em 0.8em;
+  background-color: #d9d6e0;
+  border-radius: 5px;
+  /* color: white; */
+  font-size: 13px;
+`;
+
+const Text = styled.span`
+  font-size: 1.1em;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1em;
+  height: 1em;
+  margin-left: 0.3em;
+  background-color: #d9d6e0;
+  border-radius: 50%;
+  border: none;
+`;
+
+const TagInput = styled.input`
+  /* display: inline-flex; */
+  width: 12em;
+  height: 3em;
+  margin-left: 0.4em;
+  /* background-color: red; */
+  /* background: transparent; */
+  border: none;
+  outline: none;
+  /* cursor: text; */
+`;
--- src/components/units/community/new/CommunityNew.container.tsx
+++ src/components/units/community/new/CommunityNew.container.tsx
@@ -1,18 +1,76 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useRouter } from "next/router";
 import CommunityNewUI from "./CommunityNew.presenter";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { createRef, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_BOARD } from "./CommunityNew.queries";
+
+const schema = yup.object({
+  title: yup.string().required("제목은 필수 입력 사항입니다."),
+  content: yup.string().required("내용은 필수 입력 사항입니다."),
+});
 
 export default function CommunityNew() {
   const router = useRouter();
+
+  const [tagItem, setTagItem] = useState("");
+  const [tagList, setTagList] = useState([]);
+  const [imageUrl, setImageUrl] = useState(""); //썸네일 미리보기 url
+  const [fileUrl, setFileUrl] = useState("");
+
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const { register, handleSubmit, formState, setValue, trigger } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onSubmit",
+  });
+
+  const editorRef = createRef();
+
+  const onChangeContent = () => {
+    const inputs = editorRef.current.getInstance().getHTML();
+    setValue("content", inputs);
+    trigger("content");
+  };
 
   const onClickList = () => {
     router.push("/community");
   };
 
-  const onClickSubmit = () => {
-    router.push("/community/_id");
+  const onClickSubmit = async (data) => {
+    console.log(tagList);
+    const result = await createBoard({
+      variables: {
+        createBoardInput: {
+          title: data.title,
+          content: data.content,
+          boardTags: tagList,
+          mainImg: fileUrl,
+        },
+      },
+    });
+    console.log(result);
+    // router.push("/community/_id");
   };
 
   return (
-    <CommunityNewUI onClickList={onClickList} onClickSubmit={onClickSubmit} />
+    <CommunityNewUI
+      onClickList={onClickList}
+      onClickSubmit={onClickSubmit}
+      editorRef={editorRef}
+      onChangeContent={onChangeContent}
+      register={register}
+      handleSubmit={handleSubmit}
+      formState={formState}
+      tagItem={tagItem}
+      setTagItem={setTagItem}
+      tagList={tagList}
+      setTagList={setTagList}
+      setImageUrl={setImageUrl}
+      imageUrl={imageUrl}
+      setFileUrl={setFileUrl}
+    />
   );
 }
