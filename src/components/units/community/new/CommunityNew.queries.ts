9e32b647477e31f35b04f8ef04388fa44c74b45b
hmds1606 <hmds1606@gmail.com>
2022-07-14 12:08:33 +0900

    매장 관리자, 커뮤니티 복구

---
+++ src/components/units/community/new/CommunityNew.queries.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      title
+      content
+      boardTags {
+        id
+      }
+      mainImg
+    }
+  }
+`;
