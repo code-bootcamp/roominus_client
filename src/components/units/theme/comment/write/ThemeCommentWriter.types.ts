880c61b98bf9dfa99bff721bb168282f023c2081
haneebunny <re____@naver.com>
2022-07-17 18:14:52 +0900

    [수정] feature#282 테마 댓글 수정

--- src/components/units/theme/comment/write/ThemeCommentWriter.types.ts
+++ src/components/units/theme/comment/write/ThemeCommentWriter.types.ts
@@ -1,6 +1,20 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface IUpdateThemeReviewInput {
   rank?: string;
   content?: string;
   star?: number;
   clear?: boolean;
+}
+
+export interface IThemeCommentWriteProps {
+  el?: any;
+  setIsEdit: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface IDataProps {
+  content: string;
+  star: number;
+  rank: string;
+  clear: boolean;
 }
