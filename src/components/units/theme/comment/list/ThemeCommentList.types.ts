5dad2c9d3d6e77ac8cb045d25f3882cf586b4f7c
haneebunny <re____@naver.com>
2022-07-18 22:06:51 +0900

    [수정] feature#324 댓글 타입스크립트 수정

--- src/components/units/theme/comment/list/ThemeCommentList.types.ts
+++ src/components/units/theme/comment/list/ThemeCommentList.types.ts
@@ -11,35 +11,12 @@ export interface IThemeCommentListUIItemProps {
   el: {
     id: string;
     star: number;
+    user: {
+      name?: string;
+    };
     createdAt: string;
     clear: boolean;
     rank: string;
     content: string;
-    theme?: {
-      id: string;
-      title: string;
-      rank: number;
-      intro_title: string;
-      intro_content: string;
-      agelimit: number;
-      peoplelimit: number;
-      clearTime: number;
-    };
-    user: {
-      name: string;
-    };
-  };
-}
-
-export interface IFetchThemeReviewsData {
-  id: string;
-  star: number;
-  createdAt: string;
-  clear: boolean;
-  rank: string;
-  content: string;
-  user: {
-    id: string;
-    name: string;
   };
 }
