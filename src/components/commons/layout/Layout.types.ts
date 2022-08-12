473e58bcbad6be97db9775f41b2b0232928db15e
eunchae <104378330+dev-eun-chae@users.noreply.github.com>
2022-08-12 14:41:15 +0900

    [삭제] 미사용 컴포넌트 정리

--- src/components/commons/layout/Layout.types.ts
+++ src/components/commons/layout/Layout.types.ts
@@ -3,12 +3,10 @@ import { MouseEventHandler } from "react";
 export interface IHeaderUIProps {
   activedMenu: string;
   onClickMenu: any;
-
   onClickLogout: () => Promise<void>;
 }
 
 export interface INavigationUIProps {
-  // onClickMenu: MouseEventHandler<HTMLLIElement>;
   onClickMenu: any;
   activedMenu: string;
 }
@@ -20,7 +18,6 @@ export interface IIsActiveProps {
 export interface ISideBarUIProps {
   userInfo: any;
   activedMenu: string;
-  // onClickMenu: MouseEventHandler<HTMLLIElement>;
   onClickMenu: any;
   onClickMyPage: () => void;
   onClickLogout: () => Promise<void>;
@@ -29,8 +26,6 @@ export interface ISideBarUIProps {
 export interface ISideBarAdminUIProps {
   activedMenu: string;
   onClickMenu: any;
-
-  // onClickMenu: MouseEventHandler<HTMLLIElement>;
 }
 
 export interface ITopHeaderUIProps {
